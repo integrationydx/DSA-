af49030bcd6e77933b459088996a45d299a43e61
Aditya Alur <58852980+integrationydx@users.noreply.github.com>
2026-06-29 15:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.70%) - LeetSync

---
+++ 400-nth-digit/nth-digit.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        long long digitLength = 1;
+        long long count = 9;
+        long long start = 1;
+
+        while (n > digitLength * count) {
+            n -= digitLength * count;
+            digitLength++;
+            count *= 10;
+            start *= 10;
+        }
+
+        long long number = start + (n - 1) / digitLength;
+
+        string s = to_string(number);
+
+        return s[(n - 1) % digitLength] - '0';
+    }
+};
