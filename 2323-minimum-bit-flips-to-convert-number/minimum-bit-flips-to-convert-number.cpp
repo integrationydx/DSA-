6ef414cba38c0d78bfee1b4f3b34c6fbdffb4ef0
Aditya Alur <58852980+integrationydx@users.noreply.github.com>
2026-07-04 17:49:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (59.24%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        vector<int>f;
-        vector<int>g;
-        while(start>0)
-        {
-            if(start%2 == 1)f.push_back(1);
-            else{
-                f.push_back(0);
-            }
-           start = start/2;
-        }
-        while(goal>0)
-        {
-            if(goal%2 == 1)g.push_back(1);
-            else{
-                g.push_back(0);
-            }
-            goal = goal/2;
-        }
-        while (f.size() < g.size())
-    f.push_back(0);
-
-while (g.size() < f.size())
-    g.push_back(0);
-        int count = 0;
-        for(int i =0;i<f.size();i++)
-        {
-            if(f[i]!=g[i]) count++;
-        }
-        return count;
+       int ans = start^goal;
+       int count = 0;
+       
+       for(int i = 0;i<31;i++)
+       {
+        if(ans&(1<<i)) count++;
+       }
+       return count;
+    
     }
 };
