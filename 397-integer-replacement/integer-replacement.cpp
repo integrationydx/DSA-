c77eb3dbbe55a5d1d5506d9ac8435e2ee6cb6977
Aditya Alur <58852980+integrationydx@users.noreply.github.com>
2026-07-05 16:25:51 +0530

    Time: 1 ms (32.43%) | Memory: 7.8 MB (57.77%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int solve(long long n)
+    {
+        if(n == 1)
+        {
+            return 0;
+        }
+
+        if(n % 2 == 0)
+        {
+            return 1 + solve(n / 2);
+        }
+        else
+        {
+            return 1 + min(solve(n - 1), solve(n + 1));
+        }
+    }
+
+    int integerReplacement(int n) {
+        return solve((long long)n);
+    }
+};
