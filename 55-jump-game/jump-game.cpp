e995a061763384bf87dd46adc76085cae758c978
Aditya Alur <58852980+integrationydx@users.noreply.github.com>
2026-07-18 21:11:29 +0530

    Time: 552 ms (9.33%) | Memory: 55.9 MB (6.18%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+bool fun(vector<int>& nums,int i,int n,vector<int> &dp)
+{
+    if(i >= n-1) return true;
+    if(dp[i]!=-1) return dp[i];
+    for(int jump = 1;jump<=nums[i];jump++)
+    {
+      if(fun(nums,i+jump,n,dp)) return dp[i] = true;
+    }
+    return dp[i] = false;
+}
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int mini = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(i>mini) return false;
-            mini = max(mini,i+nums[i]);
-        }
-        return true;
+       int n = nums.size();
+       vector<int>dp(n,-1);
+       return fun(nums,0,n,dp);
+     
     }
 };
